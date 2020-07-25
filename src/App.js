906e2915dd93a099bd14763db6bf45a00aff34cf
Emanuel684 <65985811+Emanuel684@users.noreply.github.com>
2020-07-24 21:13:15 -0500

    Cree mi primer proyecto en reack y modifique el app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Integrantes del PPI</h1>
+      <h2>BARPPI</h2>
+      <br></br>
+      <table>
+        <tr>
+          <td><strong>Nombre</strong></td>
+          <td><strong>Apellidos</strong></td>
+          <td><strong>Correo</strong></td>
+          <td><strong>Celular</strong></td>
+        </tr>
+        <tr>
+          <td>Emanuel</td>
+          <td>Acevedo Muñoz</td>
+          <td>emanuelacag@gmail.com</td>
+          <td>318 7604393</td>
+        </tr>
+        <tr>
+        <td>Juan Jose</td>
+          <td>Posada Garcia</td>
+          <td>jjposadagarcia@gmail.com</td>
+          <td>301 5365876</td>
+        </tr>
+        <tr>
+        <td>Bryant</td>
+          <td>Rivas Barrientos</td>
+          <td>bryantrivas18@gmail.com</td>
+          <td>313 8872348</td>
+        </tr>
+        <tr>
+        <td>Marlon Andrés</td>
+          <td>Velásquez Castro</td>
+          <td>andres311002@gmail.com</td>
+          <td>300 3520852</td>
+        </tr>
+      </table>
     </div>
   );
 }
